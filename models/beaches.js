34f2472217e34c69371e99327357e5820eda9bff
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-10-08 23:25:37 -0500

    created api/beaches route

---
+++ models/beaches.js
@@ -0,0 +1,26 @@
+// const server = require("server.js");
+const config = require('../config/config.js')
+
+// const app = express();
+// const server = require ("./server.js")
+// const axios = require("axios");
+
+// API CALL TO VISITOR CENTER (NODE AXIOS)
+
+module.exports = module.exports = function(sequelize, DataTypes) {
+	const beachInfo = sequelize.define('beachInfo', {
+		county: DataTypes.STRING,
+		siteName: DataTypes.STRING,
+		address: DataTypes.STRING,
+		latitude: DataTypes.STRING,
+		longitude: DataTypes.STRING,
+		website: DataTypes.STRING,
+		startTime: DataTypes.STRING,
+		endTime: DataTypes.STRING,
+		whatToBring: DataTypes.STRING,
+		organization: DataTypes.STRING,
+		phone: DataTypes.BOOLEAN,
+		email: DataTypes.STRING,
+	})
+	return beachInfo
+}
