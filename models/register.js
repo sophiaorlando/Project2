ecde21893fc65e2891dae5b3036e6dd911a67bef
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-10-14 00:20:44 -0500

    added register event id to table

---
+++ models/register.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+	const Register = sequelize.define('Register', {
+		eventType: {
+			type: DataTypes.BOOLEAN,
+		},
+	})
+
+	Register.associate = function(models) {
+		// We're saying that a Post should belong to an Author
+		// A Post can't be created without an Author due to the foreign key constraint
+		Register.belongsTo(models.Events, {
+			foreignKey: {
+				allowNull: false,
+			},
+		})
+
+		Register.hasMany(models.User, {
+			onDelete: 'cascade',
+		})
+	}
+
+	return Register
+}
