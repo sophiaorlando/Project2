fad441a020b0796e2cf8e5496e716e224eb758c4
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-10-14 19:27:10 -0500

    added even page, with html, css, js and route updated

--- public/js/beach.js
+++ public/js/beach.js
@@ -2,12 +2,20 @@ $(function() {
 	$('.create_btn').on('click', function(event) {
 		event.preventDefault()
 
-		const btnId = $('.create_btn').data('id')
+		const btnId = $(this).data('id')
 
 		console.log(btnId)
 
 		window.location.href = `/createevent/${btnId}`
 
 		// alert(rowId)
+	})
+
+	$('.event_item').on('click', function(event) {
+		event.preventDefault()
+
+		const eventId = $(this).data('id')
+
+		window.location.href = `/event/${eventId}`
 	})
 })
