30b62bf888244ec0ffaf4727638439924e969a2a
sophiaorlando <sophia19orlando@gmail.com>
2020-10-17 01:48:28 -0400

    updated

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: process.env.JB_url,
+    use_env_variable: "process.env.JB_url",
     dialect: "mysql",
   },
 };
