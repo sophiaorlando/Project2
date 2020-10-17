1f4a899bb06d782f83feca071fe051bbc32f6ed7
sophiaorlando <sophia19orlando@gmail.com>
2020-10-17 00:24:22 -0400

    updated config

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.JB_username,
-    password: process.env.JB_password,
-    database: process.env.JB_database,
-    host: process.env.JB_host,
+    username: "kc8ltxt5xjkywh0c",
+    password: "i8jx0yajrxrok3p1",
+    database: "iem3211s5bdww0v4",
+    host: "f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
+    username: process.env.DB_username,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    use_env_variable: process.env.JB_url,
+    use_env_variable: "mysql://kc8ltxt5xjkywh0c:i8jx0yajrxrok3p1@f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/iem3211s5bdww0v4",
     dialect: "mysql",
   },
 };
