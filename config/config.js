4c21c510ed4bb6af73d743492a80171f3640c5c1
sophiaorlando <sophia19orlando@gmail.com>
2020-10-17 01:39:18 -0400

    updated

--- config/config.js
+++ config/config.js
@@ -1,21 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: process.env.DB_username,
-    password: process.env.DB_password,
-    database: process.env.DB_database,
-    host: process.env.DB_host,
-    dialect: 'mysql'
+    username: process.env.JB_username,
+    password: process.env.JB_password,
+    database: process.env.JB_database,
+    host: process.env.JB_host,
+    dialect: "mysql",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: 'mysql'
+    dialect: "mysql",
   },
   production: {
     use_env_variable: process.env.JB_url,
-    dialect: 'mysql'
-  }
+    dialect: "mysql",
+  },
 };
